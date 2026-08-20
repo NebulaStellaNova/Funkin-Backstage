ae93e96d1195ce9bcfeb87873741b7bf739c3e01
Nebula S. Nova <jasontheone111@outlook.com>
2026-08-20 01:47:38 -0400

    Bye Bye supabase

--- api.js
+++ api.js
@@ -1,2 +1,1 @@
-const SUPABASE_URL = 'https://egaeclkieoesmalmvnky.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_0VXkntLwiTWa9S4PHdcDdg_i1F8Zth7';
+const API = 'https://api.funkinbackstage.com';
